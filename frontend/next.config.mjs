9dc53252d16a4d11a06c1ea4e5795a63ae315d48
Lucas Kim <lucaskamadakim@gmail.com>
2025-01-11 11:49:05 -0500

    Frontend setup

---
+++ frontend/next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+if (process.env.NEXT_PUBLIC_TEMPO) {
+    nextConfig["experimental"] = {
+      // NextJS 13.4.8 up to 14.1.3:
+      swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]]
+    }
+  }
+
+export default nextConfig;
